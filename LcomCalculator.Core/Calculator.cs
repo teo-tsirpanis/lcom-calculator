7e9a7ca5aa53ac282e5ae2f1da6b2719dc2df14d
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2021-04-03 13:29:20 +0300

    Track only field accesses; not properties.
    And use a lookup table to increase performance.

--- LcomCalculator.Core/Calculator.cs
+++ LcomCalculator.Core/Calculator.cs
@@ -8,18 +8,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Mono.Cecil;
-using Mono.Cecil.Cil;
 
 namespace LcomCalculator.Core
 {
     public static class Calculator
     {
-        private static IEnumerable<FieldDefinition> GetEligibleFields(TypeDefinition type) =>
-            from field in type.Fields
-            // Exclude property backing fields.
-            where !field.Name.EndsWith(">k__BackingField")
-            select field;
-
         private static IEnumerable<MethodDefinition> GetEligibleMethods(TypeDefinition type) =>
             from meth in type.Methods
             // Exclude methods without a body (abstract, extern etc).
@@ -27,7 +20,7 @@ namespace LcomCalculator.Core
             // Exclude constructors.
             where !meth.IsConstructor
             // Exclude property getters and setters.
-            where !(meth.IsSpecialName && (meth.Name.StartsWith("get_") || meth.Name.StartsWith("set_")))
+            where !(meth.IsSpecialName && (meth.Name.StartsWith("get_", StringComparison.Ordinal) || meth.Name.StartsWith("set_", StringComparison.Ordinal)))
             select meth;
 
         /// <summary>
@@ -35,38 +28,29 @@ namespace LcomCalculator.Core
         /// </summary>
         public static int CalculateLackOfCohesion(TypeDefinition type)
         {
-            var dataMembers = new List<ClassDataMember>(type.Fields.Count + type.Properties.Count);
-
-            foreach (var field in GetEligibleFields(type))
-                dataMembers.Add(new Field(field));
-            foreach (var property in type.Properties)
-                dataMembers.Add(new Property(property));
-
+            // We used to consider properties as well, tracking calls to the getters and setters,
+            // until it became apparent that property accesses from the same class directly access
+            // the backing field. Non-trivial properties will be assumed to be more complex than simple
+            // field accesses and will be ingored; as we ignore all getters and setters.
+            var fields = type.Fields;
+            var fieldLookup = new Dictionary<FieldReference, int>();
+            for (int i = 0; i < fields.Count; i++)
+                fieldLookup[fields[i]] = i;
             var methods = GetEligibleMethods(type).ToList();
 
             var methodReferenceMatrix = new BitArray[methods.Count];
             for (int i = 0; i < methodReferenceMatrix.Length; i++)
             {
-                var referencedFields = new BitArray(dataMembers.Count);
-                foreach (var instruction in type.Methods[i].Body.Instructions)
-                {
-                    for (int j = 0; j < dataMembers.Count; j++)
-                    {
-                        if (dataMembers[j].IsUsed(instruction))
-                        {
-                            referencedFields.Set(j, true);
-                            break;
-                        }
-                    }
-                }
-
-                methodReferenceMatrix[i] = referencedFields;
+                var referencedFields = methodReferenceMatrix[i] = new BitArray(fields.Count);
+                foreach (var instruction in methods[i].Body.Instructions)
+                    if (instruction.Operand is FieldReference field
+                        && fieldLookup.TryGetValue(field, out var fieldIndex))
+                        referencedFields[fieldIndex] = true;
             }
 
-            // This is Q in the LCOM definition.
             var q = 0;
             var p = 0;
-            var scratch = new BitArray(dataMembers.Count);
+            var scratch = new BitArray(fields.Count);
             for (int i = 0; i < methodReferenceMatrix.Length; i++)
                 for (int j = i + 1; j < methodReferenceMatrix.Length; j++)
                 {
